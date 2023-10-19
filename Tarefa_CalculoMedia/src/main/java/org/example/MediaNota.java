ec732f7ec5a9872f0ef18983679ed7309f3cdf82
unknown <vinyvick3@gmail.com>
2023-10-18 21:01:27 -0300

    Adicionando tarefa

---
+++ Tarefa_CalculoMedia/src/main/java/org/example/MediaNota.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class MediaNota {
+    public static void main(String[] args) {
+
+        int nota1 = 10;
+        int nota2 = 8;
+        float nota3 = 7.4F;
+        int nota4 = 4;
+        float media = (nota1 + nota2 + nota3 + nota4) / 4;
+        System.out.println("Média: " + media);
+    }
+}
