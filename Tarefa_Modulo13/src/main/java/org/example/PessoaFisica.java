71306a8fdf12d43f23ea68617e70a4f6e087cb13
unknown <vinyvick3@gmail.com>
2023-11-09 10:45:27 -0300

    Adicionando tarefa do modulo 13

---
+++ Tarefa_Modulo13/src/main/java/org/example/PessoaFisica.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class PessoaFisica extends Pessoa{
+
+    private Double cpf;
+
+    public Double getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(Double cpf) {
+        this.cpf = cpf;
+    }
+}
---
+++ Tarefa_Modulo13/src/main/java/org/example/PessoaJuridica.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class PessoaJuridica extends Pessoa{
+
+    private Double cnpj;
+
+    public Double getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(Double cnpj) {
+        this.cnpj = cnpj;
+    }
+}
