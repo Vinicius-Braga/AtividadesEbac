190c860696ae50b1abc3bd105c8bff914c331734
unknown <vinyvick3@gmail.com>
2024-01-05 12:37:55 -0300

    Ajustando tarefa Generics

--- Exercicio_Generics/src/main/java/org/example/Main.java
+++ Exercicio_Generics/src/main/java/org/example/Main.java
@@ -6,14 +6,19 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        List<String> lista = new ArrayList<>();
+        List<Carro> lista = new ArrayList<>();
 
         Honda civic = new Honda();
         civic.setNome("Civic G10");
         civic.setAno(2020);
         civic.setPlaca("ISK2E29");
+        Volkswagen gol = new Volkswagen();
+        gol.setNome("Gol G5");
+        gol.setAno(2010);
+        gol.setPlaca("ISU9R92");
 
-        lista.add(String.valueOf(civic));
+        lista.add(civic);
+        lista.add(gol);
 
 
         System.out.println(lista);
