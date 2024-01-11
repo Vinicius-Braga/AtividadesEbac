d316d94ff43c338905ab1adf7fe86ebfbeee6d5b
unknown <vinyvick3@gmail.com>
2024-01-11 15:33:25 -0300

    Adicionando tarefa de Reflections

--- Tarefa_Reflections/src/main/java/org/example/Main.java
+++ Tarefa_Reflections/src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
+
+import java.lang.annotation.*;
+
+@org.example.Tabela(value = "Funcioanrios")
 public class Main {
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Class<?> clazz = Main.class;
+
+        Annotation annotation = clazz.getAnnotation(org.example.Tabela.class);
+        String nomeTabela = ((Tabela) annotation).value();
+        System.out.println("Tabela: " + nomeTabela);
     }
 }
